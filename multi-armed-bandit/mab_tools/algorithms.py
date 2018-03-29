ad0ac0a6615b74ab083976af2b810e314b8a21e9
Yuta Saito <elpistolero317@gmail.com>
2018-03-29 19:26:01 +0900

    code reviewed

--- multi-armed-bandit/mab_tools/algorithms.py
+++ multi-armed-bandit/mab_tools/algorithms.py
@@ -4,28 +4,35 @@ import random
 import numpy as np
 
 
-def ind_max(x: list) -> int:
-    m = max(x)
-    return x.index(m)
+class MABInterface:
+    def __init__(self) -> None:
+        pass
+
+    def initialize(self) -> None:
+        pass
+
+    def select_arm(self) -> None:
+        pass
+
+    def update(self) -> None:
+        pass
 
 
-class EpsilonGreedy():
+class EpsilonGreedy(MABInterface):
     def __init__(self, epsilon: float, counts: int, values: float) -> None:
         self.epsilon = epsilon
         self.counts = counts
         self.values = values
-        return
 
     def initialize(self, n_arms: int) -> None:
-        self.counts = [0 for col in range(n_arms)]
-        self.values = [0.0 for col in range(n_arms)]
-        return
+        self.counts = [0] * n_arms
+        self.values = [0.0] * n_arms
 
     def select_arm(self) -> int:
+        result = random.randrange(len(self.values))
         if random.random() > self.epsilon:
-            return ind_max(self.values)
-        else:
-            return random.randrange(len(self.values))
+            result = np.argmax(self.values)
+        return result
 
     def update(self, chosen_arm: int, reward: float) -> None:
         self.counts[chosen_arm] += 1
@@ -34,96 +41,81 @@ class EpsilonGreedy():
         value = self.values[chosen_arm]
         new_value = (value * (n - 1) / n) + reward / n
         self.values[chosen_arm] = new_value
-        return
 
 
-def categorical_draw(probs: list) -> int:
-    z = random.random()
-    cum_prob = 0.0
-    for i in range(len(probs)):
-        prob = probs[i]
-        cum_prob += prob
-        if cum_prob > z:
-            return i
-
-    return len(probs) - 1
-
-
-class SoftMax():
+class SoftMax(MABInterface):
     def __init__(self, temperature: float, counts: int, values: float) -> None:
         self.temperature = temperature
         self.counts = counts
         self.values = values
 
     def initialize(self, n_arms: int) -> None:
-        self.counts = [0 for col in range(n_arms)]
-        self.values = [0.0 for col in range(n_arms)]
-        return
+        self.counts = [0] * n_arms
+        self.values = [0.0] * n_arms
 
     def select_arm(self) -> int:
         z = sum([math.exp(v / self.temperature) for v in self.values])
         probs = [math.exp(v / self.temperature) / z for v in self.values]
-        return categorical_draw(probs)
+        return np.random.choice(len(self.counts), p=probs)
 
     def update(self, chosen_arm: int, reward: float) -> None:
-        self.counts[chosen_arm] = self.counts[chosen_arm] + 1
+        self.counts[chosen_arm] += 1
         n = self.counts[chosen_arm]
 
         new_value = ((n - 1) / n) * self.values[chosen_arm] + (1 / n) * reward
         self.values[chosen_arm] = new_value
-        return
 
 
-class UCB1():
+class UCB1(MABInterface):
     def __init__(self, counts: int, values: float) -> None:
         self.counts = counts
         self.values = values
 
     def initialize(self, n_arms: int) -> None:
-        self.counts = [0 for col in range(n_arms)]
-        self.values = [0.0 for col in range(n_arms)]
-        return
+        self.counts = [0] * n_arms
+        self.values = [0.0] * n_arms
 
     def select_arm(self) -> int:
         n_arms = len(self.counts)
-        for arm in range(n_arms):
-            if self.counts[arm] == 0:
-                return arm
 
-        ucb_values = [0.0 for col in range(n_arms)]
-        total_counts = sum(self.counts)
-        for arm in range(n_arms):
-            bonus = math.sqrt(2 * math.log(total_counts) / self.counts[arm])
-            ucb_values[arm] = self.values[arm] + bonus
-        return ind_max(ucb_values)
+        if 0 in self.counts:
+            result = self.counts.index(0)
+        else:
+            ucb_values = [0.0] * n_arms
+            total_counts = sum(self.counts)
+            for arm in range(n_arms):
+                bonus = math.sqrt(2 * math.log(total_counts) / self.counts[arm])
+                ucb_values[arm] = self.values[arm] + bonus
+            result = np.argmax(ucb_values)
+
+        return result
 
     def update(self, chosen_arm: int, reward: float) -> None:
-        self.counts[chosen_arm] = self.counts[chosen_arm] + 1
+        self.counts[chosen_arm] += 1
         n = sum(self.counts)
 
         new_value = ((n - 1) / n) * self.values[chosen_arm] + (1 / n) * reward
         self.values[chosen_arm] = new_value
-        return
 
 
-class TompsonSampling():
-    def __init__(self, counts_alpha: float, counts_beta: float, values: float) -> None:
+class TompsonSampling(MABInterface):
+    def __init__(self, counts_alpha: float, counts_beta: float, values: float, alpha=1.0: float, beta=1.0: float) -> None:
         self.counts_alpha = counts_alpha
         self.counts_beta = counts_beta
-        self.alpha = 1
-        self.beta = 1
         self.values = values
+        self.alpha = alpha
+        self.beta = beta
 
     def initialize(self, n_arms: int) -> None:
-        self.counts_alpha = [0.0 for col in range(n_arms)]
-        self.counts_beta = [0.0 for col in range(n_arms)]
-        self.values = [0.0 for col in range(n_arms)]
+        self.counts_alpha = [0.0] * n_arms
+        self.counts_beta = [0.0] * n_arms
+        self.values = [0.0] * n_arms
 
     def select_arm(self) -> int:
         theta = [random.betavariate(self.counts_alpha[arm] + self.alpha,
                                     self.counts_beta[arm] + self.beta)
                  for arm in range(len(self.counts_alpha))]
-        return ind_max(theta)
+        return np.argmax(theta)
 
     def update(self, chosen_arm: int, reward: float) -> None:
         if reward == 1:
@@ -135,4 +127,3 @@ class TompsonSampling():
 
         new_value = ((n - 1) / n) * self.values[chosen_arm] + (1 / n) * reward
         self.values[chosen_arm] = new_value
-        return
--- multi-armed-bandit/mab_tools/test.py
+++ multi-armed-bandit/mab_tools/test.py
@@ -22,47 +22,49 @@ class BernoulliArm():
             return 1.0
 
 
-def test_algorithm(algo, arms: list, num_sims: int, horizon: int) -> list:
-    chosen_arms = np.zeros(num_sims * horizon)
-    rewards = np.zeros(num_sims * horizon)
-    cumulative_rewards = np.zeros(num_sims * horizon)
-    sim_nums = np.zeros(num_sims * horizon)
-    times = np.zeros(num_sims * horizon)
+def sim_mabs(algo_list: list, arms: list, num_sims: int, horizon: int) -> list:
+    sim_data_list = []
+    for algo in algo_list:
+        chosen_arms = np.zeros(num_sims * horizon)
+        rewards = np.zeros(num_sims * horizon)
+        cumulative_rewards = np.zeros(num_sims * horizon)
+        sim_nums = np.zeros(num_sims * horizon)
+        times = np.zeros(num_sims * horizon)
 
-    for sim in range(num_sims):
-        sim = sim + 1
-        algo.initialize(len(arms))
+        for sim in range(num_sims):
+            sim = sim + 1
+            algo.initialize(len(arms))
 
-        for t in range(horizon):
-            t = t + 1
-            index = (sim - 1) * horizon + t - 1
-            sim_nums[index] = sim
-            times[index] = t
+            for t in range(horizon):
+                t += 1
+                index = (sim - 1) * horizon + t - 1
+                sim_nums[index] = sim
+                times[index] = t
 
-            chosen_arm = algo.select_arm()
-            chosen_arms[index] = chosen_arm
+                chosen_arm = algo.select_arm()
+                chosen_arms[index] = chosen_arm
 
-            reward = arms[chosen_arms[index]].draw()
-            rewards[index] = reward
+                reward = arms[chosen_arms[index]].draw()
+                rewards[index] = reward
 
-            if t == 1:
-                cumulative_rewards[index] = reward
-            else:
-                cumulative_rewards[index] = cumulative_rewards[index - 1] + reward
+                if t == 1:
+                    cumulative_rewards[index] = reward
+                else:
+                    cumulative_rewards[index] = cumulative_rewards[index - 1] + reward
 
-            algo.update(chosen_arm, reward)
+                algo.update(chosen_arm, reward)
 
-    return [sim_nums, times, chosen_arms, rewards, cumulative_rewards]
+        sim_data = [sim_nums, times, chosen_arms, rewards, cumulative_rewards]
+        df = DataFrame({"sim_nums": sim_data[0],
+                        "times": sim_data[1],
+                        "chosen_arm": sim_data[2],
+                        "reward": sim_data[3],
+                        "cumulative_rewards": sim_data[4]
+                        }).set_index(["sim_nums", "times"])
 
+        sim_data_list.append(df)
 
-def to_frame(test_list: list) -> pd.DataFrame:
-    df = DataFrame({"sim_nums": test_list[0],
-                    "times": test_list[1],
-                    "chosen_arm": test_list[2],
-                    "reward": test_list[3],
-                    "cumulative_rewards": test_list[4]
-                    }).set_index(["sim_nums", "times"])
-    return df
+    return sim_data_list
 
 
 def average_rewards(df_list: list, name_list: list) -> None:
